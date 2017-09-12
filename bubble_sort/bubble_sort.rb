7e0a37bebf8877d3adf00b5576927b5b1d04ed71
Isil Donmez <isil.donmez@gmail.com>
2017-09-13 00:07:21 +0100

    Finish bubble_sort_by method

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -12,3 +12,19 @@ end
 
 puts bubble_sort([4,3,78,2,0,2]).inspect
 # => [0, 2, 2, 3, 4, 78]
+
+def bubble_sort_by (array)
+	(array.length - 1).downto(1) do |limit|
+		for n in (0...limit)
+			if yield(array[n], array[n+1]) > 0
+				array[n], array[n+1] = array[n+1], array[n]
+			end
+		end
+	end
+	array
+end
+
+puts (bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    	left.length - right.length
+	end).inspect
+# ["hi", "hey", "hello"]
