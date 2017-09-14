ff2de29d78653ad3c27750d8c921e0ca281ecf3e
Isil Donmez <isil.donmez@gmail.com>
2017-09-14 22:31:52 +0100

    Finish ,  and  methods

--- enumerable_methods/enumerable_methods.rb
+++ enumerable_methods/enumerable_methods.rb
@@ -1,5 +1,57 @@
 module Enumerable
 	def my_each
-		
+		for i  in [0...self.size]
+	      yield(self[i])
+	    end
+	    self
+	end
+
+	def my_each_with_index
+		for i  in [0...self.size]
+	      yield(self[i], i)
+	    end
+	    self
+	end
+
+	def my_select
+		result = []
+		for i  in [0...self.size]
+			if yield(self[i]) == true
+				result.push(self[i])
+			end
+		end
+		result
+	end
+
+	def my_all?
+		if block_given?
+		end
+	end
+# [nil, true, 99].all? #=> false
+# [].all?              #=> true
+
+	def my_any?
+		if block_given?
+		end
+	end
+#[nil, true, 99].any?   #=> true
+#[].any?                #=> false
+
+	def my_none?
+	end
+
+	def my_count
+	end
+
+	def my_map
+	end
+
+	def my_inject
+	end
+
+	def multiply_els
+	end
+
+	def my_map
 	end
 end
